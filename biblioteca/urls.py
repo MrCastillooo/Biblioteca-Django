6facac409b63abc9c076f7b4be41cbc270e62c18
MrCastillo <nanaxo1213@gmail.com>
2023-10-26 15:19:47 -0300

    extensiones

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('catalogo/admin/', admin.site.urls),
     path('catalogo/', include('catalogo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
